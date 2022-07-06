31990e31b555089f2e7342a5978fe16a98133c6e
Bryan Turek <bryan@Bryans-MacBook-Pro.local>
2022-07-06 08:49:43 -0400

    Adding ICE connection changes.

--- node/webrtc_connection.js
+++ node/webrtc_connection.js
@@ -3,6 +3,10 @@ const EventEmitter = require('events');
 const { PassThrough } = require('stream');
 const { RTCAudioSink, RTCVideoSink } = require('wrtc').nonstandard;
 
+const TIME_TO_CONNECTED = 10000;
+const TIME_TO_HOST_CANDIDATES = 3000;  // NOTE(mroberts): Too long.
+const TIME_TO_RECONNECTED = 10000;
+
 class WebRtcConnection extends EventEmitter {
 
   constructor() {
@@ -22,6 +26,17 @@ class WebRtcConnection extends EventEmitter {
     this._videoSink.addEventListener('frame', ({ frame: frame }) => {
       console.log(frame);
     });
+
+    var self = this;
+    this._connectionTimer = setTimeout(() => {
+      if (self._peerConnection.iceConnectionState !== 'connected'
+        && self._peerConnection.iceConnectionState !== 'completed') {
+          self.close();
+      }
+    }, TIME_TO_CONNECTED);
+    this._reconnectionTimer = null;
+
+    this._peerConnection.addEventListener('iceconnectionstatechange', this.onIceConnectionStateChange);
   }
 
   async createOffer() {
@@ -29,12 +44,12 @@ class WebRtcConnection extends EventEmitter {
       const offer = await this._peerConnection.createOffer();
       await this._peerConnection.setLocalDescription(offer);
       this._offer_created = true;
-      // try {
-      //   await waitUntilIceGatheringStateComplete(this._peerConnection, options);
-      // } catch (error) {
-      //   this.close();
-      //   throw error;
-      // }
+      try {
+        await this.waitUntilIceGatheringStateComplete();
+      } catch (error) {
+        this.close();
+        throw error;
+      }
     }
     return this._offer_created;
   }
@@ -45,19 +60,72 @@ class WebRtcConnection extends EventEmitter {
     }
   }
 
-  onIceConnectionStateChange() {}
+  onIceConnectionStateChange() {
+    if (this.iceConnectionState === 'connected'
+        || this.iceConnectionState === 'completed') {
+        if (this._connectionTimer) {
+          clearTimeout(this._connectionTimer);
+          this._connectionTimer = null;
+        }
+        clearTimeout(this._reconnectionTimer);
+        this._reconnectionTimer = null;
+      } else if (this.iceConnectionState === 'disconnected'
+        || this.iceConnectionState === 'failed') {
+        if (!this._connectionTimer && !this._reconnectionTimer) {
+          const self = this;
+          this._reconnectionTimer = setTimeout(() => {
+            console.log('closing connection');
+            self.close();
+          }, TIME_TO_RECONNECTED);
+        }
+      }
+  }
+
+  disableTrickleIce(sdp) {
+    return sdp.replace(/\r\na=ice-options:trickle/g, '');
+  }
+  
+  async waitUntilIceGatheringStateComplete() {
+    if (this._peerConnection.iceGatheringState === 'complete') {
+      return;
+    }
+  
+    const deferred = {};
+    deferred.promise = new Promise((resolve, reject) => {
+      deferred.resolve = resolve;
+      deferred.reject = reject;
+    });
+  
+    var self = this;
+    const timeout = setTimeout(() => {
+      self._peerConnection.removeEventListener('icecandidate', onIceCandidate);
+      deferred.reject(new Error('Timed out waiting for host candidates'));
+    }, TIME_TO_HOST_CANDIDATES);
+  
+    function onIceCandidate({ candidate }) {
+      if (!candidate) {
+        clearTimeout(timeout);
+        self._peerConnection.removeEventListener('icecandidate', onIceCandidate);
+        deferred.resolve();
+      }
+    }
+  
+    this._peerConnection.addEventListener('icecandidate', onIceCandidate);
+  
+    await deferred.promise;
+  }
 
   close() {
     this._peerConnection.removeEventListener(
       'iceconnectionstatechange', this.onIceConnectionStateChange);
-    // if (this.connectionTimer) {
-    //   options.clearTimeout(this.connectionTimer);
-    //   connectionTimer = null;
-    // }
-    // if (this.reconnectionTimer) {
-    //   options.clearTimeout(this.reconnectionTimer);
-    //   reconnectionTimer = null;
-    // }
+    if (this._connectionTimer) {
+      clearTimeout(this._connectionTimer);
+      this._connectionTimer = null;
+    }
+    if (this._reconnectionTimer) {
+      clearTimeout(this._reconnectionTimer);
+      this._reconnectionTimer = null;
+    }
     this._peerConnection.close();
   };
 
@@ -70,41 +138,6 @@ class WebRtcConnection extends EventEmitter {
     }
   }
 
-}
-
-function disableTrickleIce(sdp) {
-  return sdp.replace(/\r\na=ice-options:trickle/g, '');
-}
-
-async function waitUntilIceGatheringStateComplete(peerConnection, options) {
-  if (peerConnection.iceGatheringState === 'complete') {
-    return;
-  }
-
-  const { timeToHostCandidates } = options;
-
-  const deferred = {};
-  deferred.promise = new Promise((resolve, reject) => {
-    deferred.resolve = resolve;
-    deferred.reject = reject;
-  });
-
-  const timeout = options.setTimeout(() => {
-    peerConnection.removeEventListener('icecandidate', onIceCandidate);
-    deferred.reject(new Error('Timed out waiting for host candidates'));
-  }, timeToHostCandidates);
-
-  function onIceCandidate({ candidate }) {
-    if (!candidate) {
-      options.clearTimeout(timeout);
-      peerConnection.removeEventListener('icecandidate', onIceCandidate);
-      deferred.resolve();
-    }
-  }
-
-  peerConnection.addEventListener('icecandidate', onIceCandidate);
-
-  await deferred.promise;
 }
 
 module.exports = new WebRtcConnection();
